6198e922185ae0a2db0fb8330d994ce978011a57
abay9508 <abay.m@wellnutscorp.com>
2023-12-18 23:08:43 +0600

    create docker-compose.yaml

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # kaspi_bot
+## Getting started
+
+1. Change the config according to the local environment. The config should work with `docker-compose.yaml.example` out of the box. In order to use it:
+    1. `cp docker-compose.yaml{.example,}`
+    1. Create a folder for docker volumes: `mkdir ~/.docker-conf`
+    1. Run `docker-compose up` to run containerized project dependencies.
---
+++ cmd/app/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "context"
+  "fmt"
+  "net/url"
+  "os"
+
+  //Импортируем библиотеку для работы с postgres
+  "github.com/jackc/pgx/v4"
+)
+
+func main()  {
+  //Создание строки для подключения
+  connStr := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable&connect_timeout=%d",
+     "postgres",
+     url.QueryEscape("db_user"),
+     url.QueryEscape("pwd123"),
+     "localhost",
+     "54320",
+     "db_test",
+     5)
+
+  //Контекст с отменой
+  ctx, _ := context.WithCancel(context.Background())
+
+  //Подключение к базе данных. В случае неуспешного подключения выводим ошибку
+  conn, err := pgx.Connect(ctx, connStr)
+  if err != nil {
+     fmt.Fprintf(os.Stderr, "Connect to database failed: %v\n", err)
+     os.Exit(1)
+  }
+  fmt.Println("Connection OK!")
+
+  //Ping делает пустой запрос к базе данных (";") для проверки наличия фактического соединения
+  err = conn.Ping(ctx)
+  if err != nil {
+     fmt.Fprintf(os.Stderr, "Ping failed: %v\n", err)
+     os.Exit(1)
+  }
+  fmt.Println("Query OK!")
+
+//Закрываем соединение с базой
+conn.Close(ctx)
+
+} 
